cc19cc6c1296201d90a9db19d2ee42130809fcd2
hkdnet <satoko.itse@gmail.com>
2018-03-19 02:15:48 +0900

    Add a spec

--- spec/dummycc/parser_spec.rb
+++ spec/dummycc/parser_spec.rb
@@ -21,4 +21,24 @@ RSpec.describe DummyCC::Parser do
       ]
     end
   end
+
+  describe '/* コメント' do
+    let(:text) do
+      <<-EOS
+      int
+      /* a
+      b
+      c */
+      int
+      EOS
+    end
+
+    it do
+      expect(tokens.instance_variable_get('@tokens')).to eq [
+        DummyCC::Token.new('int', :int, 1),
+        DummyCC::Token.new('int', :int, 5),
+        DummyCC::Token.new('', :eof, 5),
+      ]
+    end
+  end
 end
