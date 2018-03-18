81101ba375f6e278c92dd71bff0be6eb9e2610db
hkdnet <satoko.itse@gmail.com>
2018-03-19 02:00:58 +0900

    mv

--- lib/dummycc/token.rb
+++ lib/dummycc/token.rb
@@ -1,21 +1,23 @@
-class Token
-  attr_reader :type, :str, :num, :lineno
+module DummyCC
+  class Token
+    attr_reader :type, :str, :num, :lineno
 
-  def initialize(str, type, lineno)
-    @str = str
-    @type = type
-    @lineno = lineno
-    if type == :digit
-      @num = str.to_i
+    def initialize(str, type, lineno)
+      @str = str
+      @type = type
+      @lineno = lineno
+      if type == :digit
+        @num = str.to_i
+      end
     end
-  end
 
-  def inspect
-    text = "#{type}: #{str}"
-    if @num
-      text << " as #{num}"
+    def inspect
+      text = "#{type}: #{str}"
+      if @num
+        text << " as #{num}"
+      end
+      text << " at #{lineno}"
+      text
     end
-    text << " at #{lineno}"
-    text
   end
 end
