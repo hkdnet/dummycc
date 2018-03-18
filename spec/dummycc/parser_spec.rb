f61935b409a040199b0028c0f121b87c5eabe231
hkdnet <satoko.itse@gmail.com>
2018-03-19 02:11:35 +0900

    Add a spec for line comment

--- spec/dummycc/parser_spec.rb
+++ spec/dummycc/parser_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 RSpec.describe DummyCC::Parser do
-  it ''
+  let(:parser) { DummyCC::Parser.new(text) }
+  let(:tokens) { parser.parse }
+
+  describe '行コメント' do
+    let(:text) do
+      <<-EOS
+      int
+      // a
+      int
+      EOS
+    end
+
+    it do
+      expect(tokens.instance_variable_get('@tokens')).to eq [
+        DummyCC::Token.new('int', :int, 1),
+        DummyCC::Token.new('int', :int, 3),
+        DummyCC::Token.new('', :eof, 3),
+      ]
+    end
+  end
 end
