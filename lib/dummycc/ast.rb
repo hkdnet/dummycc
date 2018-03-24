399f7c85b3251753dfba89f7741b32837b6f62fc
hkdnet <satoko.itse@gmail.com>
2018-03-24 17:54:14 +0900

    Add VariableDecl, BinaryExpr

--- lib/dummycc/ast.rb
+++ lib/dummycc/ast.rb
@@ -7,4 +7,26 @@ module DummyCC::AST
       @type ||= self.class.name.downcase.to_sym
     end
   end
+
+  class VariableDecl < Base
+    attr_reader :name, :decl_type
+
+    def initialize(name, decl_type)
+      unless DECL_TYPES.include?(decl_type)
+        raise DummyCC::UnknownDeclTypeError, "No such decl_type: #{decl_type}"
+      end
+      @name = name
+      @decl_type = decl_type
+    end
+  end
+
+  class BinaryExpr < Base
+    attr_reader :op, :l, :r
+
+    def initialize(op, l, r)
+      @op = op
+      @l = l
+      @r = r
+    end
+  end
 end
