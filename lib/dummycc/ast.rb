997ff1441fbf4cfce898a979ffdb2ffbb6cf972b
hkdnet <satoko.itse@gmail.com>
2018-03-24 17:59:01 +0900

    Add CallExpr

--- lib/dummycc/ast.rb
+++ lib/dummycc/ast.rb
@@ -29,4 +29,20 @@ module DummyCC::AST
       @r = r
     end
   end
+
+  class CallExpr < Base
+    attr_reader :callee
+
+    def initialize(callee, args)
+      @callee = callee
+      @args = args
+    end
+
+    def arg_at(idx)
+      if idx < 0 || idx >= @args.size
+        raise DummyCC::IndexError
+      end
+      @args[idx]
+    end
+  end
 end
--- spec/dummycc/ast_spec.rb
+++ spec/dummycc/ast_spec.rb
@@ -11,3 +11,22 @@ RSpec.describe DummyCC::AST::VariableDecl do
     end
   end
 end
+
+RSpec.describe DummyCC::AST::CallExpr do
+  describe '#arg_at' do
+    let(:call_expr) { DummyCC::AST::CallExpr.new('foo', [1, 2, 3]) }
+
+    it 'returns an arg' do
+      expect(call_expr.arg_at(0)).to eq 1
+    end
+
+    context 'with invalid index' do
+      it do
+        expect { call_expr.arg_at(-1) }.to raise_error DummyCC::IndexError
+      end
+      it do
+        expect { call_expr.arg_at(3) }.to raise_error DummyCC::IndexError
+      end
+    end
+  end
+end
