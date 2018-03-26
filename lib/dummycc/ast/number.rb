d5ac5fc457840b916c9f782db9bb3b6f4c39c23b
hkdnet <satoko.itse@gmail.com>
2018-03-26 20:00:48 +0900

    Add DummyCC::AST::Number#==
    これほかのも追加しないとあかんやつかな……

--- lib/dummycc/ast/number.rb
+++ lib/dummycc/ast/number.rb
@@ -5,5 +5,12 @@ module DummyCC::AST
     def initialize(val)
       @val = val
     end
+
+    def ==(other)
+      unless %i(type val).all? { |e| other.respond_to?(e) }
+        return false
+      end
+      type == other.type && val == other.val
+    end
   end
 end
