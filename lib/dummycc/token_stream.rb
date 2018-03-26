e2f2d5cd5b2b5941a2613a92a7d3cc6e3445693e
hkdnet <satoko.itse@gmail.com>
2018-03-26 23:48:44 +0900

    Define predicate methods

--- lib/dummycc/token_stream.rb
+++ lib/dummycc/token_stream.rb
@@ -32,6 +32,50 @@ module DummyCC
       token.num
     end
 
+    def plus?
+      token_type == :symbol && token_str == '+'
+    end
+
+    def minus?
+      token_type == :symbol && token_str == '-'
+    end
+
+    def asterisk?
+      token_type == :symbol && token_str == '*'
+    end
+
+    def slash?
+      token_type == :symbol && token_str == '/'
+    end
+
+    def semicolon?
+      token_type == :symbol && token_str == ';'
+    end
+
+    def comma?
+      token_type == :symbol && token_str == ','
+    end
+
+    def l_paren?
+      token_type == :symbol && token_str == '('
+    end
+
+    def r_paren?
+      token_type == :symbol && token_str == ')'
+    end
+
+    def l_brace?
+      token_type == :symbol && token_str == '{'
+    end
+
+    def r_brace?
+      token_type == :symbol && token_str == '}'
+    end
+
+    def symbol_eq?
+      token_type == :symbol && token_str == '='
+    end
+
     def token
       if @cur < 0 || @cur >= @tokens.size
         raise DummyCC::IndexError, "size: #{@tokens.size}, cur: #{@cur}"
